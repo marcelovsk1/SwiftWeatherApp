125933612c13ba5b8eb6049f8c9b2942432f5b61
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-11 14:33:33 -0400

    welcome view page is done

--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var locationManager = LocationManager()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            WelcomeView(locationManager: locationManager)
         }
-        .padding()
+     
     }
 }
 
--- WeatherApp/Views/WelcomeView.swift
+++ WeatherApp/Views/WelcomeView.swift
@@ -6,13 +6,40 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
 struct WelcomeView: View {
+    
+    @ObservedObject var locationManager: LocationManager
+    @State private var isAnimating = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                VStack(spacing: 20) {
+                    Text("Welcome to the iWeather")
+                        .bold()
+                        .font(.title)
+                    Text("Share your present location to provide the weather information for your area")
+                        .padding()
+                }
+                .multilineTextAlignment(.center)
+                .padding()
+                
+                LocationButton(.shareCurrentLocation) {
+                    locationManager.requestLocation()
+                }
+                .cornerRadius(30)
+                .symbolVariant(.fill)
+                .foregroundColor(.white)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+    
 }
 
+
 #Preview {
-    WelcomeView()
+    WelcomeView(locationManager: LocationManager())
 }

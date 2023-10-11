a9ee0472c2634cfcba57ad473dbd370a3a070789
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-11 13:16:11 -0400

    Initial Commit

---
+++ WeatherApp/Managers/LocationManager.swift
@@ -0,0 +1,32 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Marcelo Amaral Alves on 2023-10-11.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUptadeLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false 
+    }
+    
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Marcelo Amaral Alves on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

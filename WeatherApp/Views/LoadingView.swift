56e2800657f79edbaf4a2eb071e574e20e182737
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-11 14:50:42 -0400

    basic UI implemented

---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Marcelo Amaral Alves on 2023-10-11.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}

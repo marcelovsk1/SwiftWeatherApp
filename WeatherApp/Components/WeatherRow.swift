97dbd516fdf701f5e54d7cfdebba32d72fdb5288
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-13 08:18:56 -0400

    implementing weatherrow and finishing with all components

---
+++ WeatherApp/Components/WeatherRow.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherRow.swift
+//  WeatherApp
+//
+//  Created by Marcelo Amaral Alves on 2023-10-13.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherRow()
+}
